366547ed3a98d3306de7363bd8512f680abe7927
Shankaran312 <shankaranpopz@gmail.com>
2025-02-15 11:21:08 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,2 +1,2 @@
-BOT_TOKEN = 'MTI1ODc4OTg2NjI5NzM2MDQ2NQ.GOhTgh.3LA_rFHbrqLxhua32bUkienrlChHcgYqVvzExE'
-WEBSITE_URL = 'https://www.gs4u.net/en/s/307393.html'
+BOT_TOKEN = 'MTMyNjkxMTY5NTI1MTE3NzYwNQ.GpCXHs.nxnbotyFv5Zis6HvtzuexL_3gZSUnplxsFwfB8'
+WEBSITE_URL = 'https://www.gs4u.net/en/s/374501.html'
